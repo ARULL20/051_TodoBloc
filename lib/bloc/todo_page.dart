fb739bf93ec44faec8e9ee8e07c5590bd75a21f1
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-05-08 21:48:14 +0700

    tambah tampilan pada Data selector

---
+++ lib/bloc/todo_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_with_bloc/bloc/todo_bloc.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Todo List'),
+                            Row(
+                children: [
+                  Column(
+                    children: [
+                      Text('Selected Date'),
+
